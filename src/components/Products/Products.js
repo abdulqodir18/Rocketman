3a4058da1af7111db01c6bda23aef558cd780a7a
abdulqodir18 <ukkosha45@gmail.com>
2021-05-31 11:33:44 +0500

    table templete done

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,30 @@
+import "./Products.scss"
+import Table from "../Table/Table"
+
 function Products() {
    return(
-      <>Products...</>
+      <>
+         <Table>
+            <thead>
+               <tr>
+                  <th>Salom</th>
+                  <th>Salom</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>alik</td>
+                  <td>alik</td>
+               </tr>
+               <tr>
+                  <td className="pg-column" colSpan="2">
+                     <button className="pg-column__btn" aria-label="prev"></button>
+                     <button className="pg-column__btn" aria-label="next"></button>
+                  </td>
+               </tr>
+            </tbody>
+         </Table>
+      </>
    )
 }
 
