e9a7539c1c5b264ef1fa69f80d053ad726cf48b8
jakhongirs <j.sagdullaev31@gmail.com>
2021-06-01 14:06:04 +0500

    Orders top section is finished

--- src/components/Lib/Svgs/ArrowLeft.js
+++ src/components/Lib/Svgs/ArrowLeft.js
@@ -1,10 +1,27 @@
 function ArrowLeft() {
-  return (
-    <svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1.54175 6.22868L14.0417 6.22868" stroke="#200E32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M6.58325 11.249L1.54159 6.22896L6.58325 1.20813" stroke="#200E32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>    
-  )
+	return (
+		<svg
+			width='15'
+			height='12'
+			viewBox='0 0 15 12'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path
+				d='M1.54175 6.22868L14.0417 6.22868'
+				stroke='#200E32'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+			<path
+				d='M6.58325 11.249L1.54159 6.22896L6.58325 1.20813'
+				stroke='#200E32'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</svg>
+	);
 }
 
-export default ArrowLeft
+export default ArrowLeft;
