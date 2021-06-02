0d64e29cef7f67f8011c6f3f2bd9384c14aca2db
doramakh <anvarovna28@gmail.com>
2021-06-02 13:38:00 +0500

    create orders page

---
+++ src/components/Lib/Svgs/MoreDots.js
@@ -0,0 +1,11 @@
+function MoreDots() {
+	return (
+		<svg width="22" height="4" viewBox="0 0 22 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="2" cy="2" r="2" fill="#131212"/>
+        <circle cx="11" cy="2" r="2" fill="#131212"/>
+        <circle cx="20" cy="2" r="2" fill="#131212"/>
+        </svg>
+	)
+}
+
+export default MoreDots;
