790fa258698534daf01e3cb71889437b2885eb2a
mrshuhrat <shuhratadm1n@gmail.com>
2021-06-04 10:26:10 +0500

    Create settings linked lidt

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,6 +1,24 @@
+import { NavLink, Switch, Route, useRouteMatch } from 'react-router-dom'
+
 function Settings() {
+   let { path, url } = useRouteMatch();
+
    return(
-      <>Settings...</>
+      <div className="settings">
+         <div>
+            <NavLink to={url}>User</NavLink>
+            <NavLink to={`${url}/driver`}>Driver</NavLink>
+         </div>
+
+         <Switch>
+          <Route exact path={path}>
+            <>User</>
+          </Route>
+          <Route path={`${path}/driver`}>
+            <>Driver</>
+          </Route>
+        </Switch>
+      </div>
    )
 }
 
